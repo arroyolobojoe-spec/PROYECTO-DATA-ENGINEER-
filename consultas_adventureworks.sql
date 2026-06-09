79d6cf6f80af1ac268379fbd71d007f6a81e7844
arroyolobojoe-spec <arroyolobojoe@gmail.com>
2026-06-09 10:46:04 -0500

    Add SQL queries for product and customer addresses

--- consultas_adventureworks.sql
+++ consultas_adventureworks.sql
@@ -9,3 +9,20 @@ SELECT CompanyName, FirstName, LastName, Phone
 FROM Customer
 ORDER BY CompanyName ASC
 
+## BUSCA LA BICICLETA DE COLOR NEGRO ##
+SELECT Name, ProductNumber, Color, ListPrice
+FROM Product
+WHERE color = 'Black'
+
+## QUE CLIENTE TIENE UNA DIRECCION REGISTRADA EN EL SISTEMA, Y CUAL ES EL ID DE ESA DIRECCION ##
+-- REPORTE: Cruce de clientes con sus direcciones asignadas para logística
+-- Pregunta: ¿Qué clientes tienen una dirección registrada en el sistema y cuál es el ID de esa dirección?
+SELECT Customer.CustomerID, Customer.FirstName, Customer.LastName, CustomerAddress.AddressID
+FROM Customer
+INNER JOIN CustomerAddress 
+ON Customer.CustomerID = CustomerAddress.CustomerID;
+
+## Cuántas direcciones tenemos registradas por cada ID de cliente?##
+SELECT CustomerID, COUNT(AddressID) AS TOTALDIRECCION
+FROM CustomerAddress
+GROUP BY customerid;
